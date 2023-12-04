7b1f4062ab2ba263d17f2722b80984d6f1a7cc0d
r3dph0enix <ipasechnikov@gmail.com>
2023-12-04 13:05:06 +0300

    3 sulution

--- 8 kyu/convertbooleantostring.js
+++ 8 kyu/convertbooleantostring.js
@@ -1,8 +1,23 @@
 /* Implement a function which convert the given boolean value into its string representation.
 
 Note: Only valid inputs will be given. */
-
+// 1-й вариант через возврат строки и условие
 function booleanToString(b){
     //your code here
-    
+    if(b) {
+        return "true";
+    } else {
+        return "false";
+    }
   }
+
+/* 2-й вариант через конкатенацию с пустой строкой 
+function booleanToString(b) {
+    return b + "";
+} */
+
+// 3 вариант через встроенный метод toString
+
+function booleanToString {
+    return b.toSring();
+}
